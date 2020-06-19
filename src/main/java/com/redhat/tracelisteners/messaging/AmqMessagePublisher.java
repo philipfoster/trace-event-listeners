65e6d081da6ec41fa3dca761c7b85aec9754cb05
Philip <pfoster@redhat.com>
2020-06-19 10:58:14 -0500

    Messages are now successfully published to AMQ

--- src/main/java/com/redhat/tracelisteners/messaging/AmqMessagePublisher.java
+++ src/main/java/com/redhat/tracelisteners/messaging/AmqMessagePublisher.java
@@ -1,44 +1,44 @@
 package com.redhat.tracelisteners.messaging;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redhat.eventmodel.events.ProcessTraceEvent;
 import com.redhat.eventmodel.events.RuleTraceEvent;
 import com.redhat.eventmodel.events.WorkingMemoryTraceEvent;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jms.*;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-
-import static com.redhat.tracelisteners.conf.ConfigManager.getJndiPassword;
-import static com.redhat.tracelisteners.conf.ConfigManager.getJndiUser;
-
-public class AmqMessagePublisher implements MessagePublisher, AutoCloseable {
+public class AmqMessagePublisher implements MessagePublisher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AmqMessagePublisher.class);
-    private static final String JNDI_CONNECTION_FACTORY = "myFactoryLookup";
-    private static final String JNDI_PROCESS_EVENTS_QUEUE = "process-queue";
+    private static final String JNDI_CONNECTION_FACTORY = "java:/RemoteJmsXA";
+    private static final String JNDI_PROCESS_EVENTS_QUEUE = "java:/queue/testQueue";
     private static final String JNDI_RULE_EVENTS_QUEUE = "rule-queue";
     private static final String JNDI_WORKING_MEMORY_QUEUE = "working-memory-queue";
-    private static final int DELIVERY_MODE = DeliveryMode.NON_PERSISTENT;
 
-    private final Context context;
-    private final Connection connection;
+    private InitialContext context;
+    private ConnectionFactory connectionFactory;
     private final ObjectMapper mapper;
 
     public AmqMessagePublisher() throws Exception {
         // TODO: Find better way to  create the connection.
         // I think that PAM will create a new instance of our publisher for each process instance. If this is the case, this is a
         // very inefficient use of connections, and will likely cause performance issues when running under load.
-        context = new InitialContext();
         mapper = new ObjectMapper();
 
-        ConnectionFactory factory = (ConnectionFactory) context.lookup(JNDI_CONNECTION_FACTORY);
+        LOGGER.info("Initializing JMS connection factory");
+        context = new InitialContext();
+        connectionFactory = (ConnectionFactory) context.lookup(JNDI_CONNECTION_FACTORY);
 
-        connection = factory.createConnection(getJndiUser(), getJndiPassword());
-        connection.setExceptionListener(new AmqExceptionListener());
-        connection.start();
+        LOGGER.info("Successfully loaded JMS connection factory from JNDI");
     }
 
     @Override
@@ -61,22 +61,29 @@ public class AmqMessagePublisher implements MessagePublisher, AutoCloseable {
     }
 
     private void publishMessage(String destName, Object event) throws PublishingFailedException {
-        try (Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
-            Destination dest = (Destination) context.lookup(destName);
-            MessageProducer producer = session.createProducer(dest);
-
+        try {
             String eventStr = mapper.writeValueAsString(event);
+            System.out.println("DEST " + destName + " event = " + eventStr);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
-            TextMessage message = session.createTextMessage(eventStr);
-            producer.send(message, DELIVERY_MODE, Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
-        } catch (Exception e) {
-            LOGGER.error("Failed to publish message to queue " + destName, e);
+        try (Connection connection = connectionFactory.createConnection()) {
+            try (Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)){
+                Destination dest = (Destination) context.lookup(destName);
+                MessageProducer producer = session.createProducer(dest);
+
+                String eventStr = mapper.writeValueAsString(event);
+
+                TextMessage message = session.createTextMessage(eventStr);
+                producer.send(message);
+            } catch(Exception e){
+                LOGGER.error("Failed to publish message to queue " + destName, e);
+                throw new PublishingFailedException(e);
+            }
+        } catch (JMSException e) {
+            LOGGER.error("Failed to obtain JMS connection", e);
             throw new PublishingFailedException(e);
         }
-    }
-
-    @Override
-    public void close() throws Exception {
-        connection.close();
     }
 }
